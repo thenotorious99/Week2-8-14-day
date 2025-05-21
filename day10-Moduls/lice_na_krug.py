3a3dd3d6c51fa2735497057ad849fb84f6693a77
braqn1999 <117543750+braqn1999@users.noreply.github.com>
2025-05-20 22:19:20 -0500

    Day10: Moduls

---
+++ day10-Moduls/lice_na_krug.py
@@ -0,0 +1,7 @@
+import math
+
+r = int(input("Enter number: "))
+
+radius = math.pi * r ** 2
+
+print(f"Circle area: {round(radius, 2)}")
---
+++ day10-Moduls/number_from_squear.py
@@ -0,0 +1,5 @@
+import math
+
+num = float(input("Enter Number: "))
+
+print(f"Number square: {math.sqrt(num)}")
